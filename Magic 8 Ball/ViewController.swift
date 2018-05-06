d9a0c289e3e8734dd07a27583411f295a29d8773
thanhbang <thanhbang.it.dn2000@gmail.com>
2018-05-06 22:25:35 +0900

    done app

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var askMeImageView: UIImageView!
+    
+    let arrayImages = ["ball1","ball2","ball3","ball4","ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onClickAskMe(_ sender: Any) {
+        updateImages()
+        randomMax(maxValue: 10)
+    }
+    
+    func randomMax(maxValue value: Int){
+        let value : Int = Int(arc4random_uniform(UInt32(value)))
+        print("Value is \(value)")
+    }
+    
+    func random() -> Int{
+        let randomeValue = Int(arc4random_uniform(4))
+        print(randomeValue)
+        return randomeValue
+    }
+    
+    func updateImages(){
+        askMeImageView.image = UIImage(named: arrayImages[random()])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateImages()
+    }
 }
 
